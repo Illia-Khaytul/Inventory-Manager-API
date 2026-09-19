d8e3d0e5b2fc1d0cf0347cac8a919ccd4bd937dc
Illia Khaytul <khaytul87@gmail.com>
2026-09-19 15:22:12 +0200

    feat: configure JWT authentication components to use loaded RSA keys and role based authorities

---
+++ src/main/java/io/github/khaytul_illia/inventory_manager_api/security/jwt/JwtConfig.java
@@ -0,0 +1,40 @@
+package io.github.khaytul_illia.inventory_manager_api.security.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(RSAPrivateKey privateKey, RSAPublicKey publicKey){
+        return NimbusJwtEncoder.withKeyPair(publicKey, privateKey).build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAPublicKey publicKey){
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+}
