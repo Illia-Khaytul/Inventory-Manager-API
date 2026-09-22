908ac25858929f2fe03b86ca24ad8fb9133db141
Illia Khaytul <khaytul87@gmail.com>
2026-09-22 10:46:31 +0200

    feat: define User entity and its persistence

---
+++ src/main/java/io/github/khaytul_illia/inventory_manager_api/user/User.java
@@ -0,0 +1,35 @@
+package io.github.khaytul_illia.inventory_manager_api.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public enum UserRole{
+        CUSTOMER, OPERATOR
+    }
+
+}
---
+++ src/main/java/io/github/khaytul_illia/inventory_manager_api/user/UserRepository.java
@@ -0,0 +1,9 @@
+package io.github.khaytul_illia.inventory_manager_api.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
